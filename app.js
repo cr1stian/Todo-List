86d3927e91f76b152973a7631491923e278a8879
Cristian Herrera <cristianherrera@Cristians-MacBook-Pro.local>
2017-09-01 21:41:12 -0700

    Completed

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.set('views','./views');
 app.set('view engine', 'mustache')
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-
+//empty arrays for storing strings
 var todo = [];
 var done = [];
 
-
+//rending the arrays
 app.get('/', function(request, response){
 
   response.render('index',{
@@ -21,9 +21,9 @@ app.get('/', function(request, response){
     done: done
 
   })
-
+console.log(todo)
 });
-
+//pushing the List values to the Todo array and keeping user on the same page
 app.post('/', function(request, response){
 
   todo.push(request.body.list)
@@ -32,18 +32,15 @@ app.post('/', function(request, response){
 
 
 app.post('/completed', function(request, response){
-  const remove = request.body.completed
-  todo.splice(todo.indexOf(remove), 1)
-  done.push(remove)
-  response.redirect('/')
-  console.log(remove)
+  const remove = request.body.completed //creating var for complete tasks
+  todo.splice(todo.indexOf(remove), 1) //removing completed task from Todo array
+  done.push(remove) //pushing task to Done array
+  response.redirect('/') //keeping user on the same page(no refresh)
 });
 
 
 
 
-
-
-app.listen(3000, function(){
+app.listen(3000, function(){ //node is looking for port 3000 and function proves it's
   console.log('Live From the Gutter');
 });
